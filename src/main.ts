8a6276dd76cefc93ed4048a32e0dfe9c318bc102
morfouesse <amorfouesse@asi.fr>
2025-05-20 12:06:59 +0200

    add vuetify + vite suport

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
-
+app.use(
+  createVuetify({
+    components,
+    directives,
+  }),
+)
 app.use(createPinia())
 app.use(router)
 
