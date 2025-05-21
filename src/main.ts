3bd21c5b3e4ef6e1332fefb84a4df49ffe352609
morfouesse <amorfouesse@asi.fr>
2025-05-21 16:42:21 +0200

    fix fonts

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,21 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
 
 const app = createApp(App)
 app.use(
   createVuetify({
     components,
     directives,
+      icons: {
+    defaultSet: 'mdi',
+  },
   }),
 )
 app.use(createPinia())
