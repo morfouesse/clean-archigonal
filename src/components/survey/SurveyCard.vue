6a506e7d0c380580608d1ddacab0ca358dea027b
morfouesse <amorfouesse@asi.fr>
2025-05-28 10:58:48 +0200

    remove useless

--- src/components/survey/SurveyCard.vue
+++ src/components/survey/SurveyCard.vue
@@ -3,7 +3,7 @@
     <template v-slot:title>
       <span class="title">{{ survey.label }}</span>
     </template>
-    <v-list v-model:opened="open">
+    <v-list>
       <v-list-group>
         <template v-slot:activator="{ props }">
           <v-list-item v-bind="props" class="title" title="Voir les questions"></v-list-item>
@@ -22,30 +22,10 @@
   </v-card>
 </template>
 <script setup lang="ts">
-import { FetchSurveyPresenterImpl } from '@/domains/survey/adapters/fetchSurvey.presenter.impl'
-import { SurveyRepositoryFetch } from '@/domains/survey/adapters/survey.repository.fetch'
 import type { SurveyViewModel } from '@/domains/survey/ports/fetchSurvey.presenter'
-import { FetchSurveyUsecase } from '@/domains/survey/ports/fetchSurvey.usecase'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 const { survey } = defineProps<{
   survey: SurveyViewModel
 }>()
-const open = ref(['Users'])
-const surveys = ref<SurveyViewModel[]>([])
-
-const fetchSurvey = async (): Promise<void> => {
-  const fetchSurveyUsecase = new FetchSurveyUsecase(new SurveyRepositoryFetch())
-  // callback dans presenter => quand besoin dans le template
-  //sinon objet classique avec attribut d'objet
-  await fetchSurveyUsecase.execute(
-    new FetchSurveyPresenterImpl((viewModel) => {
-      surveys.value = viewModel
-    }),
-  )
-}
-
-onMounted(async () => {
-  await fetchSurvey()
-})
 </script>
